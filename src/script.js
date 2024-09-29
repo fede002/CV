3cd50958f8ec1c4217554daa6dec7c6aaef2585c
Eduardo Federico Barrios <eduardo.barrios@valtech.com>
2024-09-29 15:28:09 -0300

    cambios script de nube

--- src/script.js
+++ src/script.js
@@ -8,24 +8,24 @@ function cargaNube() {
     // Array con las habilidades y sus puntuaciones
     const skills = [
         ['Desarrollo web back-end', 9],
-        ['Magento', 8],
-        ['Microsoft SQL Server', 7],
-        ['SQL', 8],
-        ['Desarrollo de software', 7],
         ['PHP', 9],
         ['Laravel', 9],
-        ['Api Rest', 6],
-        ['HTML', 8],
-        ['JavaScript', 5],
-        ['.NET framework', 4],
-        ['ASP.NET', 3],
+        ['Magento', 8],
+        ['SQL', 8],
         ['MySQL', 8],
-        ['CSS', 4],
+        ['Api Rest', 8],
+        ['Desarrollo de software', 7],
+        ['Microsoft SQL Server', 7],
+        ['HTML', 7],
+        ['JavaScript', 6],
         ['HTML5', 5],
-        ['jQuery', 4],
-        ['C#', 3],
-        ['SAP HANA', 2],
-        ['ABAP jr', 2]
+        ['jQuery', 5],
+        ['.NET framework', 4],
+        ['CSS', 4],
+        ['C#', 4],
+        ['SAP HANA', 3],
+        ['ABAP jr', 3],
+        ['ASP.NET', 3],
     ];
 
     // Función para crear un elemento span con el tamaño de fuente correspondiente
@@ -33,10 +33,10 @@ function cargaNube() {
         const span = document.createElement('span');
         span.textContent = skill;
         span.style.fontSize = score / 5 + 'vw';
-        span.style.padding = '5px';
+        span.style.padding = '14px';
         span.style.color = 'black';
         span.style.backgroundColor = '#f2f2f2';
-        span.style.borderRadius = '5px';
+        span.style.borderRadius = '17px';
         span.style.display = 'inline-block';
         span.style.margin = '5px';
         return span;
